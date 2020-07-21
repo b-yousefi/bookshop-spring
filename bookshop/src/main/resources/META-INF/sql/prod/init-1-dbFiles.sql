9a714e7acee4fec3afde32fba8c1a6dbec84268a
b-yousefi <b.yousefi2911@gmail.com>
2020-07-22 02:25:17 +0430

    [feature] Dockerize the application

---
+++ bookshop/src/main/resources/META-INF/sql/prod/init-1-dbFiles.sql
@@ -0,0 +1,142 @@
+INSERT INTO `db_bookshop`.`dbfile`
+(`id`,
+ `data`,
+ `file_name`,
+ `file_type`)
+VALUES
+(1,
+ LOAD_FILE('/images/JaneAusten.jpg'),
+ 'Jane Austen',
+ 'Image');
+/*---------------------------------------------------------*/
+INSERT INTO `db_bookshop`.`dbfile`
+(`id`,
+ `data`,
+ `file_name`,
+ `file_type`)
+VALUES
+(2,
+ LOAD_FILE('/images/AntoineDeSaintExupery.jpg'),
+ 'Antoine de Saint-Exupéry',
+ 'Image');
+/*---------------------------------------------------------*/
+INSERT INTO `db_bookshop`.`dbfile`
+(`id`,
+ `data`,
+ `file_name`,
+ `file_type`)
+VALUES
+(3,
+ LOAD_FILE('/images/GeorgeOrwell.jpg'),
+ 'George Orwell',
+ 'Image');
+/*---------------------------------------------------------*/
+INSERT INTO `db_bookshop`.`dbfile`
+(`id`,
+ `data`,
+ `file_name`,
+ `file_type`)
+VALUES
+(4,
+ LOAD_FILE('/images/GeorgeMartin.jpg'),
+ 'George R. R. Martin',
+ 'Image');
+/*---------------------------------------------------------*/
+INSERT INTO `db_bookshop`.`dbfile`
+(`id`,
+ `data`,
+ `file_name`,
+ `file_type`)
+VALUES
+(5,
+ LOAD_FILE('/images/JKRowling.jpg'),
+ 'J. K. Rowling',
+ 'Image');
+/*---------------------------------------------------------*/
+INSERT INTO `db_bookshop`.`dbfile`
+(`id`,
+ `data`,
+ `file_name`,
+ `file_type`)
+VALUES
+(6,
+ LOAD_FILE('/images/AlaindeBotton.jpg'),
+ 'Alain de Botton',
+ 'Image');
+/*---------------------------------------------------------*/
+INSERT INTO `db_bookshop`.`dbfile`
+(`id`,
+ `data`,
+ `file_name`,
+ `file_type`)
+VALUES
+(7,
+ LOAD_FILE('/images/PrideAndPrejudice.jpg'),
+ 'Pride And Prejudice',
+ 'Image');
+/*---------------------------------------------------------*/
+INSERT INTO `db_bookshop`.`dbfile`
+(`id`,
+ `data`,
+ `file_name`,
+ `file_type`)
+VALUES
+(8,
+ LOAD_FILE('/images/SenseAndSensibility.jpg'),
+ 'Sense and Sensibility',
+ 'Image');
+/*---------------------------------------------------------*/
+INSERT INTO `db_bookshop`.`dbfile`
+(`id`,
+ `data`,
+ `file_name`,
+ `file_type`)
+VALUES
+(9,
+ LOAD_FILE('/images/Persuasion.jpg'),
+ 'Persuasion',
+ 'Image');
+/*---------------------------------------------------------*/
+INSERT INTO `db_bookshop`.`dbfile`
+(`id`,
+ `data`,
+ `file_name`,
+ `file_type`)
+VALUES
+(10,
+ LOAD_FILE('/images/Emma.jpg'),
+ 'Emma',
+ 'Image');
+/*---------------------------------------------------------*/
+INSERT INTO `db_bookshop`.`dbfile`
+(`id`,
+ `data`,
+ `file_name`,
+ `file_type`)
+VALUES
+(11,
+ LOAD_FILE('/images/TheLittlePrince.jpg'),
+ 'The Little Prince',
+ 'Image');
+/*---------------------------------------------------------*/
+INSERT INTO `db_bookshop`.`dbfile`
+(`id`,
+ `data`,
+ `file_name`,
+ `file_type`)
+VALUES
+(12,
+ LOAD_FILE('/images/AnimalFarm.jpg'),
+ 'Animal Farm',
+ 'Image');
+/*---------------------------------------------------------*/
+INSERT INTO `db_bookshop`.`dbfile`
+(`id`,
+ `data`,
+ `file_name`,
+ `file_type`)
+VALUES
+(13,
+ LOAD_FILE('/images/NineteenEighty-Four.jpg'),
+ 'Nineteen Eighty-Four',
+ 'Image');
